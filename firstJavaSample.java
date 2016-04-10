38e4109660b57f96d73cb3094be7049f50526a94
cheruby <cheruby>
2016-04-10 21:13:58 +0800

    my first selenium script

---
+++ firstJavaSample.java
@@ -0,0 +1,84 @@
+
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.support.ui.Select;
+
+public class firstJavaSample {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+	FirefoxProfile profile = new FirefoxProfile();
+	profile.setPreference("network.proxy.type", 1);
+	profile.setPreference("network.proxy.socks", "localhost");
+	profile.setPreference("network.proxy.socks_port", 8888);
+    driver = new FirefoxDriver(profile);
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testFirstJavaSample() throws Exception {
+	System.out.println("before");
+    driver.get("http://docs.seleniumhq.org/");
+    System.out.println("after");
+    driver.findElement(By.linkText("Download")).click();
+    driver.findElement(By.linkText("Release Notes")).click();
+    assertEquals("SeIDE Release Notes ¡¤ SeleniumHQ/selenium Wiki ¡¤ GitHub", driver.getTitle());
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
